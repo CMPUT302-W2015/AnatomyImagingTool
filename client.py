2bf0d0017ac4564a99271ef50b3e5d34b8160c1a
rcockbur <rcockbur@ualberta.ca>
2015-03-08 16:00:08 -0600

    added server and client

---
+++ client.py
@@ -0,0 +1,11 @@
+import sysconfig    
+from socket import socket, AF_INET, SOCK_DGRAM
+
+SERVER_IP = '10.19.216.65'
+PORT_NUMBER = 5000
+SIZE = 1024
+
+socket = socket(AF_INET, SOCK_DGRAM)
+while True:
+    socket.sendto('cool', (SERVER_IP, PORT_NUMBER))
+sys.exit()
